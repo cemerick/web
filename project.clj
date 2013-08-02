fea88b9bd1c36ef20230eba10959d97269b26bda
Chas Emerick <chas@cemerick.com>
2013-08-01 23:56:58 -0400

    ring 1.2.0 final

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.cemerick/web "0.0.2"
+(defproject com.cemerick/web "0.0.3"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.5"]
-                 [ring/ring-core "1.2.0-beta2"]
+                 [ring/ring-core "1.2.0"]
                  [enlive "1.1.1"]])
