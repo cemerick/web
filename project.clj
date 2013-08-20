d898f266423588efc5f2ccf5e3b3204684609c0b
Chas Emerick <chas@cemerick.com>
2013-08-20 14:11:45 -0400

    bump enlive

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.cemerick/web "0.0.4"
+(defproject com.cemerick/web "0.0.5"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.5"]
                  [ring/ring-core "1.2.0"]
-                 [enlive "1.1.1"]])
+                 [enlive "1.1.2"]])
